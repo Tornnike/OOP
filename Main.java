d4a7ff87fd2f72e96cce9d581083e6015a85c45c
Tornnike <36266467+Tornnike@users.noreply.github.com>
2021-11-20 11:20:56 +0400

    Add files via upload
    savarjishoebi

---
+++ Main.java
@@ -0,0 +1,96 @@
+package com.company;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+
+
+
+
+
+
+
+        int myArray[] = {1,2,3,4,5};
+
+        double mySecondArray[] = new double[5];
+
+
+        int aa = 10;
+        int bb = 20;
+
+        mySecondArray[0]= aa;
+        mySecondArray[1]= bb;
+        mySecondArray[2]= aa*bb;
+        mySecondArray[3]= aa/bb;
+        mySecondArray[4]= aa%bb;
+
+
+        for (int i = 0; i <= mySecondArray.length; i++) {
+
+            System.out.println(mySecondArray[i]);
+
+        }
+
+
+
+
+
+
+
+
+        // for each
+        for (double keso : mySecondArray) {
+
+            System.out.println(keso);
+
+        }
+
+
+        for (double v : mySecondArray) {
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
